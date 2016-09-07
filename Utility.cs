4d60d9efd8d13433eb7efa66ff30e7f57949dc5a
Sanket Patel <sanket972528@gmail.com>
2016-09-07 10:36:59 +0530

    c# utility function to resize image with less storage space
    Resize image with less storage space
    maintain good quality

---
+++ Utility.cs
@@ -0,0 +1,29 @@
+public static void ResizeImageAndSave(Bitmap image, int maxWidth, int maxHeight, int quality, string filePathNewLocation)
+        {
+            // Get the image's original width and height
+            int originalWidth = image.Width;
+            int originalHeight = image.Height;
+
+            // To preserve the aspect ratio
+            float ratioX = (float)maxWidth / (float)originalWidth;
+            float ratioY = (float)maxHeight / (float)originalHeight;
+            float ratio = Math.Min(ratioX, ratioY);
+
+            // New width and height based on aspect ratio
+            int newWidth = (int)(originalWidth * ratio);
+            int newHeight = (int)(originalHeight * ratio);
+
+            // Convert other formats (including CMYK) to RGB.
+            Bitmap newImage = new Bitmap(newWidth, newHeight, PixelFormat.Format24bppRgb);
+
+            // Draws the image in the specified size with quality mode set to HighQuality
+            using (Graphics graphics = Graphics.FromImage(newImage))
+            {
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                graphics.DrawImage(image, 0, 0, newWidth, newHeight);
+            }
+            image.Dispose();
+            newImage.Save(filePath, ImageFormat.Jpeg);
+        }
